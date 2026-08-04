1f1372e126d4f026242240f9823cbeddb4d4b14d
maxiw26-source <maxiw26@gmail.com>
2026-08-04 13:57:52 +0200

    Call create_tables function on startup

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from einkaufsliste_mit_login import app
+from einkaufsliste_mit_login import app, create_tables
+
+create_tables()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
