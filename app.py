9ba0c91ba2a701351826a56d15983edebc5f4e28
maxiw26-source <maxiw26@gmail.com>
2026-08-05 13:21:09 +0200

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from routes import app, create_tables
-
-create_tables()
-
-if __name__ == "__main__":
-    import os
-
-    port = int(os.environ.get("PORT", "5000"))
-    app.run(host="0.0.0.0", port=port, debug=False)
