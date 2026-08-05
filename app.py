6d9d375e531b1e763328db3c96fb0e7e242bebf4
maxiw26-source <maxiw26@gmail.com>
2026-08-05 15:00:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import os
 
-from einkaufplus import app, create_tables
+from application import app, create_tables
 
-# Tabellen und neue Spalten automatisch anlegen.
 create_tables()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", "5000"))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port)
